01cb13752df5f7aff269b96bc38613f2f3273977
yurkkon <103900885+yurkkon@users.noreply.github.com>
2022-06-17 19:14:22 +0200

    Update RandomNumber.java

--- RandomNumber.java
+++ RandomNumber.java
@@ -2,7 +2,7 @@ public class RandomNumber {
     public static void main(String[] args) {
 
 
-        //this simple program give you a random number from 0 to 10.
+        //this simple program gives you a random number from 0 to 10.
         int randomNum = (int) (Math.random() * 10);
         System.out.println("Your random number is " + randomNum);
 
